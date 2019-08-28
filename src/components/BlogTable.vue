f160be8037395b7788b9adcb0119063227c023c8
zhujiawen <zhujiawen123>
2019-08-28 15:56:02 +0800

    ignore

--- src/components/BlogTable.vue
+++ src/components/BlogTable.vue
@@ -71,15 +71,15 @@
       </el-button>
       <span></span>
       <div>
-      <el-pagination
-        background
-        :page-sizes="[1, 3, 5, 7]"
-        :page-size="pageSize"
-        layout="total, sizes, prev, pager, next, jumper"
-        :total="totalCount"
-        @size-change="handleSizeChange"
-        @current-change="currentChange" v-show="this.articles.length>0">
-      </el-pagination>
+        <el-pagination
+          background
+          :page-sizes="[1, 3, 5, 7]"
+          :page-size="pageSize"
+          layout="total, sizes, prev, pager, next, jumper"
+          :total="totalCount"
+          @size-change="handleSizeChange"
+          @current-change="currentChange" v-show="this.articles.length>0">
+        </el-pagination>
       </div>
     </div>
   </div>
@@ -88,10 +88,10 @@
 <script>
   import {deleteRequest, putRequest} from '../utils/api'
   import {getRequest} from '../utils/api'
- // import Vue from 'vue'
- // var bus = new Vue()
+  // import Vue from 'vue'
+  // var bus = new Vue()
 
-  export default{
+  export default {
     data() {
       return {
         pageSize: 3, //每页显示20条数据
@@ -117,13 +117,13 @@
       })
     },
     methods: {
-      searchClick(){
+      searchClick() {
         this.loadBlogs(1, this.pageSize);
       },
-      itemClick(row){
+      itemClick(row) {
         this.$router.push({path: '/blogDetail', query: {aid: row.id}})
       },
-      deleteMany(){
+      deleteMany() {
         var selItems = this.selItems;
         for (var i = 0; i < selItems.length; i++) {
           this.dustbinData.push(selItems[i].id)
@@ -131,7 +131,7 @@
         this.deleteToDustBin(selItems[0].state)
       },
       //翻页
-      currentChange(currentPage){
+      currentChange(currentPage) {
         this.currentPage = currentPage;
         this.loading = true;
         this.loadBlogs(currentPage, this.pageSize);
@@ -141,7 +141,7 @@
         this.pageSize = val;
         this.loadBlogs(this.currentPage, this.pageSize);
       },
-      loadBlogs(page, count){
+      loadBlogs(page, count) {
         var _this = this;
         var url = '';
         debugger
@@ -151,11 +151,11 @@
         //   url = "/article/all?state=" + this.state + "&page=" + page + "&count=" + count + "&keywords=" + this.keywords;
         // }
         url = `/api/article/page?pageSize=${this.pageSize}&pageNo=${this.currentPage}&keywords=${this.keywords}`;
-        getRequest(url).then(resp=> {
+        getRequest(url).then(resp => {
           debugger
           _this.loading = false;
           if (resp.status == 200) {
-            if (resp.data.code == '2000'){
+            if (resp.data.code == '2000') {
               _this.count = resp.data.data.total;
               // _this.categories = resp.data.data.rows;
               // _this.items = resp.data.data.rows
@@ -165,14 +165,14 @@
           } else {
             _this.$message({type: 'error', message: '数据加载失败!'});
           }
-        }, resp=> {
+        }, resp => {
           _this.loading = false;
           if (resp.response.status == 403) {
             _this.$message({type: 'error', message: resp.response.data});
           } else {
             _this.$message({type: 'error', message: '数据加载失败!'});
           }
-        }).catch(resp=> {
+        }).catch(resp => {
           //压根没见到服务器
           _this.loading = false;
           _this.$message({type: 'error', message: '未找到服务器!'});
@@ -182,13 +182,13 @@
         this.selItems = val;
       },
       handleEdit(index, row) {
-        this.$router.push({path: '/editBlog', query: {from: this.activeName,id:row.id}});
+        this.$router.push({path: '/editBlog', query: {from: this.activeName, id: row.id}});
       },
       handleDelete(index, row) {
         this.dustbinData.push(row.id);
         this.deleteToDustBin(row.state);
       },
-      deleteToDustBin(state){
+      deleteToDustBin(state) {
         var _this = this;
         this.$confirm(state != 2 ? '将该文件放入回收站，是否继续?' : '永久删除该文件, 是否继续?', '提示', {
           confirmButtonText: '确定',
@@ -200,7 +200,7 @@
           for (var i = 0; i < selItems.length; i++) {
             ids += selItems[i] + ",";
           }
-          if (ids.length > 0){
+          if (ids.length > 0) {
             ids = ids.substring(0, ids.length - 1);
           }
           _this.loading = true;
@@ -211,19 +211,19 @@
           } else {
             url = "/api/article/";
           }
-          deleteRequest(url + ids).then(resp=> {
-            if (resp.status == 200 && resp.data.code==2000) {
+          deleteRequest(url + ids).then(resp => {
+            if (resp.status == 200 && resp.data.code == 2000) {
               var data = resp.data;
               _this.$message({type: 'success', message: '删除成功!'});
               // if (data.status == 'success') {
-                window.bus.$emit('blogTableReload')//通过选项卡都重新加载数据
+              window.bus.$emit('blogTableReload')//通过选项卡都重新加载数据
               // }
             } else {
               _this.$message({type: 'error', message: '删除失败!'});
             }
             _this.loading = false;
             _this.dustbinData = []
-          }, resp=> {
+          }, resp => {
             _this.loading = false;
             _this.$message({type: 'error', message: '删除失败!'});
             _this.dustbinData = []
